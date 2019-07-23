18ba31fc646c48a58c4f13ed8c99afe3ec3a9e47
Audrea Cook <audreacwork@gmail.com>
2019-07-23 19:43:26 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,15 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    
+  end
 
 end
