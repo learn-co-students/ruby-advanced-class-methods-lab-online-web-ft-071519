7a8ed21f82782560d3efa739426e30b5c773660e
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-28 20:19:19 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,16 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  @@create
   def self.all
     @@all
   end
-
+def self.create(song)
+  @@create = @@all << self 
+end 
   def save
     self.class.all << self
   end
 
 end
+end 
